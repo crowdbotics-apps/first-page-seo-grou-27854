3bcb89eebe043eea2f31a41f21a4fa1a842c0687
Crowdbotics <team@crowdbotics.com>
2021-06-08 22:07:19 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/first_page_seo_grou_27854/MainActivity.java
@@ -0,0 +1,15 @@
+package com.first_page_seo_grou_27854;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "first_page_seo_grou_27854";
+  }
+}
